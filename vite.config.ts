2abda27d4cd6c829b712d14e2e3628b2c66d10c2
Mike Jonas <motion@gmail.com>
2024-09-30 00:27:13 -0700

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Use '/' if deploying to root of GitHub Pages
+  base: "/portfolio-site/", // Set this to match your repository name
 });
