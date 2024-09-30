4c01ebf5cfaa673729fc54056725bcbd74ca17ea
Mike Jonas <motion@gmail.com>
2024-09-30 00:30:20 -0700

    Fix config for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio-site/", // Set this to match your repository name
+  base: "/", // "/" for custom domain
 });
